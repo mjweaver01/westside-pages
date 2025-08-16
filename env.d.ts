e8d51fad3bc487f656eabb9c422c3e2cf44c3f28
mjweaver01 <8439630+mjweaver01@users.noreply.github.com>
2025-08-16 14:45:26 -0400

    Build fixes?

--- env.d.ts
+++ env.d.ts
@@ -5,3 +5,9 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+
+declare module '*.astro' {
+  import type { AstroComponentFactory } from 'astro/runtime/server/index.js'
+  const Component: AstroComponentFactory
+  export default Component
+}
